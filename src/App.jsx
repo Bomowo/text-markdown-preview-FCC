68e4435c5908c63e6ee827920cf9052c45f9e763
Bomowo <oeccc25@yahoo.com>
2023-10-09 21:44:58 +0300

    Initial HTML and func

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-
+import {useState} from 'react'
 
 function App() {
+  const [text, setText] = useState('Hello, World!')
 
+  function changeTextArea (e) {
+    setText(e.target.value)
+  }
 
   return (
     <>
-      <h1>Hello, World !</h1>
+      <div id="preview" className='preview'>{text}</div>
+      <textarea id='editor' 
+                onKeyDown={changeTextArea} 
+                value={text} 
+                onChange={changeTextArea}/>
     </>
   )
 }
